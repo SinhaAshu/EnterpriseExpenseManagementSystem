845a36ceb1682623ba832647bbfff6a1f7df1c19
SinhaAshu <ashusinha1999@gmail.com>
2025-02-24 20:16:11 +0530

    pushing the handlelogin class

---
+++ BackendEEMS/src/main/java/com/zidioProject/eems/Entity/HandleLogin.java
@@ -0,0 +1,20 @@
+package com.zidioProject.eems.Entity;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HandleLogin {
+	
+	private String username;
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+}
