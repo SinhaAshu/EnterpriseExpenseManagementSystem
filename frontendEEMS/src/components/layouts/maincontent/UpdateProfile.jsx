e972daeab82192c649d6bfa2d188ec8fb52904e1
SinhaAshu <ashusinha1999@gmail.com>
2025-04-17 18:40:46 +0530

    added update profile and linked to the backend

--- frontendEEMS/src/components/layouts/maincontent/UpdateProfile.jsx
+++ frontendEEMS/src/components/layouts/maincontent/UpdateProfile.jsx
@@ -1,30 +1,133 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '@/components/styles/homelayout.css'
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { logoutUser } from '../../../utils/auth';
+import { useNavigate } from 'react-router-dom'; 
 
 const UpdateProfile = () => {
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem('token');
+    if(!token){
+        navigate('/');
+    }
+
+   const [formData, setFormData] = useState(
+    { full_name: ''});
+
+   const [email, setEmail] = useState(
+    { email: ''});
+
+   const [password, setPassword] = useState(
+    { password: ''});
+
+   const handleemail = (e) => {
+    setEmail({
+      ...email, [e.target.name]: e.target.value
+    });
+  };
+
+  const handlepassword = (e) => {
+    setPassword({
+      ...password, [e.target.name]: e.target.value
+    });
+  };
+
+  const handlechange = (e) => {
+    setFormData({
+      ...formData, [e.target.name]: e.target.value
+    });
+  };
+
+   const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try{
+        const response = await axios.put('http://localhost:8080/api/dashboard/profile/update', formData, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        if(response.data){
+            alert("Updated Successfully!");
+            setFormData({
+                full_name: ''
+            });
+        }
+    }catch(error){
+        toast.error("Something unsual happened! Please try again!");
+        console.error("Error:", error);
+    }
+   };
+
+   const handleChangeEmail = async (e) =>{
+    e.preventDefault();
+    const confirmEmail = window.confirm("Are you sure you want to change your email?");
+    if (!confirmEmail) return;
+    try{
+        const response = await axios.put('http://localhost:8080/api/dashboard/profile/update-email', email, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        if(response.data){
+            alert("Updated Successfully!");
+            setEmail({
+                email: ''
+            });
+            logoutUser();
+        }
+    }catch(error){
+        toast.error("Something unsual happened! Please try again!");
+        console.error("Error:", error);
+    }
+   };
+
+   const handleChangePassword = async (e) =>{
+    e.preventDefault();
+    const confirmPassword = window.confirm("Are you sure you want to change your password?");
+    if (!confirmPassword) return;
+    try{
+        const response = await axios.put('http://localhost:8080/api/dashboard/profile/update-password', password, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        if(response.data){
+            alert("Updated Successfully!");
+            setPassword({
+                password: ''
+            });
+            logoutUser();
+        }
+    }catch(error){
+        toast.error("Something unsual happened! Please try again!");
+        console.error("Error:", error);
+    }
+   };
+   
+
     return (
         <>
             <h2 className='emp-updateprofile-heading'>Update Your Profile</h2>
             <div className='emp-updateprofile'>
-                <form className='editprofile-form'>
+                <form className='editprofile-form' onSubmit={handleSubmit}>
 
                     <label>Full Name:</label>
-                    <input type="text" placeholder='Enter full name'></input>
-
-                    <label>Email:</label>
-                    <input type="email" placeholder='Enter new email address'></input>
-
-                    <label>Password:</label>
-                    <input type="password" placeholder='Enter your new password'></input>
-
-                    <label>Choose Role:</label>
-                    <select>
-                        <option value="">Employee</option>
-                        <option value="">Manager</option>
-                    </select>
-
+                    <input type="text" placeholder='Enter full name' name = "full_name" value={formData.full_name} 
+                     onChange={handlechange} required></input>
+                     
                     <button className='saveupdate-btn' type='submit'>Save</button>
 
+                    <label>Email:</label>
+                    <input type="email" placeholder='Enter new email address' name= "email" value={email.email}
+                    onChange={handleemail}></input>
+                    <button className='submit-btn' onClick={handleChangeEmail}>Change Email</button>
+
+                    <label>Password:</label>
+                    <input type="password" placeholder='Enter your new password' name= "password" value={password.password}
+                    onChange={handlepassword}></input>
+                    <button className='submit-btn' onClick={handleChangePassword}>Change Password</button>
                 </form>
             </div>
         </>
